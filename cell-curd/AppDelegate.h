ce8401d7c57c09a1a28a31525641baf989773dd1
necfol <necfoll@gmail.com>
2017-09-04 20:45:32 +0800

    init

---
+++ cell-curd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cell-curd
+//
+//  Created by necfol on 2017/9/4.
+//  Copyright © 2017年 Necfol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
